2831eb98844c695ff761d66114fcf4f160f87940
Nicolas Morales <n.morales.0@proton.me>
2023-10-28 20:55:53 -0700

    pull the docker image and clone the script repo

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const { exec } = require("child_process")
+
+function shell_exec(cmd) {
+  exec("cmd", (error, stdout, stderr) => {
+    if (error) {
+      throw new Error('error: ${error.message}')
+    }
+    if (stderr) {
+        console.log('${stderr}');
+    }
+    console.log('${stdout}');
+  });
+}
 
 try {
   const package_name = core.getInput('package_name')
@@ -9,9 +22,23 @@ try {
 
   const env_image = 'gitea.arcturuscollective.com/arcturus-collective/linux-' + build_compiler + '-' + build_arch + ':latest'
 
-  const version = fs.readFileSync('VERSION', 'utf8')
+  const version = fs.readFileSync('VERSION', 'utf8').trim()
 
-  console.log(`Building ${package_name} Version ${version} with compiler ${build_compiler} on ${build_arch}.`);
+  console.log(`Building dependencies for ${package_name} Version ${version} with compiler ${build_compiler} on ${build_arch}.`);
+
+  const build_script = '.ac_build/scripts/dependencies-linux-' + '-' + build_arch + '.sh';
+  const gitea_username = core.getInput('username')
+  const gitea_password = core.getInput('password')
+
+  if (gitea_username && gitea_password)
+  {
+    shell_exec('git -C .ac_build pull || git clone https://' + gitea_username + ':' + gitea_password + '@gitea.arcturuscollective.com/arcturus-collective/drone-templates.git .ac_build')
+  } else {
+    shell_exec('git -C .ac_build pull || git clone https://gitea.arcturuscollective.com/arcturus-collective/drone-templates.git .ac_build')
+  }
+
+  shell_exec('docker pull ' + env_image)
+
 } catch (error) {
   core.setFailed(error.message);
 }
