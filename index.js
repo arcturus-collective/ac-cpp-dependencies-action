d06873490ed8fb4b4b62edff61d3bd941a83eb9a
Nicolas Morales <n.morales.0@proton.me>
2023-10-29 13:05:45 -0700

    execute the given script depending on the os

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const github = require('@actions/github');
 const fs = require('fs');
 const { exec } = require("child_process")
 
-function shell_exec(cmd) {
+function shell_exec(cmd, env = {}) {
   console.log(`Running command ${cmd}`)
-  exec(cmd, (error, stdout, stderr) => {
+  runenv = { ...process.env, ...env };
+  exec(cmd, { env: runenv }, (error, stdout, stderr) => {
     if (error) {
       throw new Error(error.message)
     }
@@ -21,13 +22,32 @@ try {
   const build_arch = core.getInput('arch');
   const build_compiler = core.getInput('compiler');
 
-  const env_image = `gitea.arcturuscollective.com/arcturus-collective/linux-${build_compiler}-${build_arch}:latest`
+  // Get the OS we are running on
+  let build_os = "";
+  let script_exec = "";
+  let script_ext = "";
+  switch (process.platform)
+  {
+    case "linux":
+      build_os = "linux";
+      script_exec = "bash";
+      script_ext = ".sh";
+      break;
+    case "win32":
+      build_os = "windows";
+      script_exec = "powershell -File";
+      script_ext = ".ps1";
+    default:
+      throw new Error(`unsupported platform ${process.platform}`)
+  }
+
+  const env_image = `gitea.arcturuscollective.com/arcturus-collective/${build_os}-${build_compiler}-${build_arch}:latest`
 
   const version = fs.readFileSync('VERSION', 'utf8').trim()
 
-  console.log(`Building dependencies for ${package_name} Version ${version} with compiler ${build_compiler} on ${build_arch}.`);
+  console.log(`Building dependencies for ${package_name} Version ${version} with compiler ${build_compiler} on ${build_os} ${build_arch}.`);
 
-  const build_script = `.ac_build/scripts/dependencies-linux-${build_arch}.sh`;
+  const build_script = `.ac_build/scripts/dependencies-${build_os}-${build_arch}.${script_ext}`;
   const gitea_username = core.getInput('username');
   const gitea_password = core.getInput('password');
   const cwd = process.cwd();
@@ -39,16 +59,8 @@ try {
     shell_exec('git -C .ac_build pull || git clone https://gitea.arcturuscollective.com/arcturus-collective/drone-templates.git .ac_build')
   }
 
-/*
-  shell_exec(`docker pull ${env_image}`)
-
-  function docker_cmd(cmd) {
-    shell_exec(`docker run --rm -v ${cwd}:/opt/src/${package_name} --env PACKAGE_NAME=${package_name} --env PACKAGE_VERSION=${version} -w /opt/src/${package_name} ${env_image} ${cmd}`)
-  }
-
-  docker_cmd(`ls -la`)
-  docker_cmd(`bash ${build_script}`)
-  */
+  // Now actually execute the script
+  shell_exec(`${script_exec} ${build_script}`, {PACKAGE_NAME: package_name, PACKAGE_VERSION: version});
 
 } catch (error) {
   core.setFailed(error.message);
