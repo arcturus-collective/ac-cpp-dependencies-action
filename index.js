315df92248e89a7b12bc40450d538564498dc7ad
Nicolas Morales <n.morales.0@proton.me>
2023-10-28 17:16:07 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+try {
+  const package_name = core.getInput('package_name')
+  const build_arch = core.getInput('arch');
+  const build_compiler = core.getInput('compiler');
+
+  const env_image = 'gitea.arcturuscollective.com/arcturus-collective/linux-' + compiler + '-' + arch + ':latest'
+
+  const version = fs.readFileSync('VERSION', 'utf8')
+
+  console.log(`Building ${package_name} Version ${version} with compiler ${build_compiler} on ${build_arch}.`);
+} catch (error) {
+  core.setFailed(error.message);
+}
